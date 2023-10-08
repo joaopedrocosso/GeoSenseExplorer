bd4d02234ba0def5a9ecfc95c6e110e4a1d77414
João Pedro Cosso <astrojcosso@gmail.com>
2023-10-08 17:07:51 -0300

    first commit

---
+++ js/request.js
@@ -0,0 +1,22 @@
+// Sending and receiving data in JSON format using POST method
+//
+
+//Pegar dados do HTML e passar pra função abaixo
+
+function postRequestLatLon(lat, lon) {
+    var xhr = new XMLHttpRequest();
+    var url = "https://nasa-api-sij3k2u3xa-rj.a.run.app/nasa/writeTif";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.status); //Enviar esse dado pra uma div ou algo do tipo pra ser exibido
+        }
+    };
+    var data = JSON.stringify({"lat": lat, "lon": lon});
+    xhr.send(data);
+}
+
+//Chamar a função no front, num event OnClick ou algo do tipo
+//postRequestLatLon(lat,lon)
